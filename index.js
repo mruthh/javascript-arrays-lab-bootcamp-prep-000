6c562abc046f8c7e7c17d7783085968bf15e618a
Megan H <meganhonig@gmail.com>
2017-10-02 20:55:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-function destructivelyAppendKitten(arr) {
-  arr.push("tiny kitten");
-  return arr;
+function destructivelyAppendKitten(name) {
+  name.push("tiny kitten");
+  return name;
 }
