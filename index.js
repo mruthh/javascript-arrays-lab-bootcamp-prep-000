aee0af431d6ac65d0433d24713aec4f9102cb0f4
Megan H <meganhonig@gmail.com>
2017-10-02 20:59:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"].
-function destructivelyAppendKitten(name) {
-  name.push("tiny kitten");
+function destructivelyAppendKitten() {
+  push("tiny kitten");
   return name;
 }
